d6fc499f8df7df049cfec2b54374ee36a50ab09d
Idrees Khan <dotnetdreamer@users.noreply.github.com>
2020-03-28 14:29:40 +0400

    fixed resources bug

--- expense-tracker-web/src/main.ts
+++ expense-tracker-web/src/main.ts
@@ -1,6 +1,8 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
+// process.env.TZ = 'utc';
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { cors: true });
   await app.listen(3000);
