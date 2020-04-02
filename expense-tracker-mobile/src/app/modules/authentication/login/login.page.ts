38b343432556e438944be512e1c8e9723d8c5d3a
Idrees Khan <dotnetdreamer@users.noreply.github.com>
2020-04-03 03:21:47 +0400

    Update login.page.ts

--- expense-tracker-mobile/src/app/modules/authentication/login/login.page.ts
+++ expense-tracker-mobile/src/app/modules/authentication/login/login.page.ts
@@ -1,6 +1,10 @@
 import { Component, OnInit, ViewChild, ElementRef, AfterViewInit } from '@angular/core';
 
+import { LoginType, ILoginParams, IUser } from '../authentication.model';
 import { AuthenticationGoogleService } from '../authentication-google.service';
+import { UserSettingService } from '../user-setting.service';
+import { EventPublisher } from '../../shared/event-publisher';
+import { UserConstant } from '../user-constant';
 
 @Component({
   selector: 'app-login',
@@ -10,19 +14,21 @@ import { AuthenticationGoogleService } from '../authentication-google.service';
 export class LoginPage implements OnInit, AfterViewInit {
   @ViewChild('gSigninButton') gSigninButton: ElementRef;
 
-  constructor(private googleAuthSvc: AuthenticationGoogleService) { }
+  constructor(private gAuthService: AuthenticationGoogleService
+    , private userSettingSvc: UserSettingService
+    , private eventPub: EventPublisher) { }
 
   ngOnInit() {
   }
 
   async ngAfterViewInit() {
-    this.googleAuthSvc.init(this.gSigninButton.nativeElement
-      , (gUserProfile) => {
-
-        console.log(gUserProfile);
-
-    //   document.getElementById('name').innerText = "Signed in: " + googleUser.getBasicProfile().getName();
-      }, (e: { error: 'popup_closed_by_user' }) => {
+    this.gAuthService.init(this.gSigninButton.nativeElement
+      , async (gUserProfile) => {
+        await this._handleLogin({ 
+          loginType: LoginType.GOOGLE, 
+          user: gUserProfile 
+        });
+      }, (e) => {
         console.log(e);
       });
   }
@@ -31,4 +37,31 @@ export class LoginPage implements OnInit, AfterViewInit {
     // await this.googleAuthSvc.init(this.gSigninButton.nativeElement);
   }
 
+  
+  private async _handleLogin(args: ILoginParams) {
+    const promises = [];
+
+    let profilePromise = this.gAuthService.putUserProfileLocal(args.user);
+    promises.push(profilePromise);
+
+    let loginTypePromise = this.userSettingSvc.putLoggedInMethod(args.loginType);
+    promises.push(loginTypePromise);
+
+    if(args.loginType == LoginType.STANDARD) {
+      let currentUserPromise = this.userSettingSvc.putCurrentUser(args.email);
+      promises.push(currentUserPromise);
+
+      //TODO: need to remvoe storing password
+      let passwordPromise =  this.userSettingSvc.putCurrentUserPassword(args.password);
+      promises.push(passwordPromise);
+    } else {
+      let currentUserPromise = this.userSettingSvc.putCurrentUser(args.user.email);
+      promises.push(currentUserPromise);
+    }
+
+    await Promise.all(promises);
+    
+    //fire the user loggedin event
+    this.eventPub.$pub(UserConstant.EVENT_USER_LOGGEDIN, args.user);
+  }
 }
