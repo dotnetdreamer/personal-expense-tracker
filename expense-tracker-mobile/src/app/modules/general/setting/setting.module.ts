2a59ad4285079aa5e691619ff38e4b45dea7593a
Idrees Khan <dotnetdreamer@users.noreply.github.com>
2020-05-30 13:03:02 +0400

    add directive to settings

--- expense-tracker-mobile/src/app/modules/general/setting/setting.module.ts
+++ expense-tracker-mobile/src/app/modules/general/setting/setting.module.ts
@@ -6,10 +6,12 @@ import { SettingPageRoutingModule } from './setting-routing.module';
 import { SettingPage } from './setting.page';
 import { PipesModule } from 'src/app/pipes/pipes.module';
 import { ComponentsWithFormsModule } from 'src/app/components/components-with-forms.module';
+import { DirectivesModule } from 'src/app/directives/directives.module';
 
 @NgModule({
   imports: [
     PipesModule,
+    DirectivesModule,
     ComponentsWithFormsModule,
     SettingPageRoutingModule
   ],
